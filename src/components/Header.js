07fccfb23ea835cd7303498bc8f1a79cc2918001
DevBassel <bassel444555@gmail.com>
2024-04-16 22:17:57 +0200

    add active link

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,6 @@ import { useLocale, useTranslations } from "next-intl";
 import { tokenContext } from "../app/Context/tokenContext";
 
 function Header() {
-  const t = useTranslations();
   const logo = "/assets/images/L3.3.png";
   const CartImg = "/assets/images/cart.png";
   const locale = useLocale();
@@ -25,37 +24,6 @@ function Header() {
   console.log(pathname);
   let ttt = useContext(tokenContext);
   const [isClient, setIsClient] = useState(false);
-
-  const NavLinks = [
-    {
-      name: t("header_link_home"),
-      link: "/",
-    },
-    {
-      name: t("header_link_shop"),
-      link: "/store",
-    },
-    {
-      name: t("header_link_draws"),
-      link: "/draws",
-    },
-    {
-      name: t("header_link_winners"),
-      link: "/winners",
-    },
-    {
-      name: t("header_link_blog"),
-      link: "/blog",
-    },
-    {
-      name: t("header_link_about"),
-      link: "/about-us",
-    },
-    {
-      name: t("header_link_contact"),
-      link: "/contact",
-    },
-  ];
 
   useEffect(() => {
     setIsClient(true);
@@ -72,6 +40,7 @@ function Header() {
   const { totalItems, items } = useCart();
   const [search, setSearch] = useState(false);
   const [state, setState] = useState(false);
+  const t = useTranslations();
   const [info, setInfo] = useState({});
 
   const user =
@@ -138,19 +107,55 @@ function Header() {
             </Link>
 
             <div className="links">
-              {NavLinks.map((item) => {
-                return (
-                  <Link
-                    key={item.name}
-                    className={`me-2 nav-links ${
-                      item.link === pathname ? "active" : ""
-                    }`}
-                    href={item.link}
-                  >
-                    {item.name}
-                  </Link>
-                );
-              })}
+              <Link
+                className={`me-2 nav-links ${pathname === "/" ? "active" : ""}`}
+                href={"/"}
+              >
+                {t("header_link_home")}
+              </Link>
+              <Link
+                className={`me-2 nav-links ${
+                  pathname.startsWith("/store") ? "active" : ""
+                } `}
+                href={"/store"}
+              >
+                {t("header_link_shop")}
+              </Link>
+              <Link
+                className={`me-2 nav-links ${
+                  pathname.startsWith("/draws") ? "active" : ""
+                } `}
+                href={"/draws"}
+              >
+                {t("header_link_draws")}
+              </Link>
+              <Link
+                className={`me-2 nav-links ${
+                  pathname.startsWith("/winners") ? "active" : ""
+                } `}
+                href={"/winners"}
+              >
+                {t("header_link_winners")}
+              </Link>
+              {/* <Link className="me-2 nav-links" href="/blog">
+              {t("header_link_blog")}
+            </Link> */}
+              <Link
+                className={`me-2 nav-links ${
+                  pathname.startsWith("/about-us") ? "active" : ""
+                } `}
+                href={"/about-us"}
+              >
+                {t("header_link_about")}
+              </Link>
+              <Link
+                className={`me-2 nav-links ${
+                  pathname.startsWith("/contact") ? "active" : ""
+                } `}
+                href={"/contact"}
+              >
+                {t("header_link_contact")}
+              </Link>
             </div>
             <div className="operation">
               <div
--- src/components/home/slider/HomeSlide.js
+++ src/components/home/slider/HomeSlide.js
@@ -3,47 +3,88 @@
 import React from "react";
 import "swiper/css";
 import "swiper/css/navigation";
-import "@/src/Style/SlideInfo.scss";
 import "@/src/Style/HomeSlide.css";
 import "@/src/Style/products.css";
 import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import { Link } from "../../../utils/navigation";
+import Box from "@mui/material/Box";
+import { useTranslations } from "next-intl";
 
 function HomeSlide({ data, sl }) {
+  const media = window.matchMedia("(max-width: 991px)");
+  const t = useTranslations();
   return (
     <div className="home-slide">
       <div className="container">
         <div className="slider">
           <Swiper
             modules={[Pagination, Navigation]}
+            spaceBetween={40}
             slidesPerView={1}
             loop={true}
-            navigation={true}
+            navigation
             pagination={{ clickable: true }}
           >
             {data?.map((el) => {
               return (
-                <SwiperSlide key={`Slide_${el.id}`} className="slide-info">
-                  <div className="row slide-x01 ">
-                    <div className="col-lg-6 col-md-12 slide-x02 d-flex justify-content-lg-end justify-content-center maxw-lg-40">
-                      <div className="text mx-5 ">
-                        <h2 className="slider_head">{el?.title}</h2>
+                <SwiperSlide key={el.id} className="slide-info">
+                  <div
+                    className="row slide-x01"
+                    style={{ justifyContent: "center" }}
+                  >
+                    <Box
+                      className="col-lg-6 col-md-12 slide-x02"
+                      sx={{
+                        //  backgroundColor: { lg: "yellow" },
+                        maxWidth: { lg: "40%" },
+                        display: "flex",
+                        justifyContent: { lg: "flex-start !important" },
+                      }}
+                    >
+                      <Box
+                        className="text mx-5"
+                        sx={{
+                          margin: {
+                            lg: "0rem !important",
+                          },
+                        }}
+                      >
+                        <h2 className="slider_head text-white">{el?.title}</h2>
                         <div className="mobile_img">
                           <img src={el?.photo} alt="" />
                         </div>
-                        <p className="slider_info">{el?.description}</p>
-                        <Link href={`${el?.link}`} className="more-info">
-                          {sl}
+                        <p
+                          className="slider_info"
+                          dangerouslySetInnerHTML={{
+                            __html:
+                              media.matches === true
+                                ? el?.description?.slice(0, 30)
+                                : el?.description,
+                          }}
+                        ></p>
+                        <Link
+                          href={`${el?.link}`}
+                          className="more-info text-white "
+                        >
+                          {t("slider_link")}
                         </Link>
-                      </div>
-                    </div>
-                    <div className="col-lg-6 col-md-12 slide-x03 d-flex  justify-content-center maxw-lg-40">
+                      </Box>
+                    </Box>
+                    <Box
+                      className="col-lg-6 col-md-12 slide-x03"
+                      sx={{
+                        //  backgroundColor: { lg: "black" },
+                        maxWidth: { lg: "40%" },
+                        display: "flex",
+                        justifyContent: { lg: "flex-end !important" },
+                      }}
+                    >
                       <div className="image">
                         <img src={el?.photo} alt="" />
                       </div>
-                    </div>
+                    </Box>
                   </div>
                 </SwiperSlide>
               );
